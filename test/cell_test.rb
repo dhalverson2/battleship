c5e1fd4bc0702a427eb08c581f663f716a678829
Austin Aspaas <araspaas@live.com>
2020-07-06 19:10:41 -0600

    Add empty test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -18,4 +18,10 @@ class CellTest < Minitest::Test
     assert_equal "B4", cell.coordinate
     assert_nil(cell.ship)
   end
+
+  def test_it_starts_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
 end
