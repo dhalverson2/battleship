5018e2cb4469a0585fccdd51593669777d8eda44
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-06 18:24:55 -0600

    Add ship attributes test and attr_reader

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -11,5 +11,11 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
+  def test_it_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
 
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+    assert_equal 3, cruiser.health
+  end
 end
