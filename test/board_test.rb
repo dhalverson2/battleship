df98bff05c68a14d7d335d808cd5293e0a4b149d
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-07 20:23:18 -0600

    Add consecutive coords test

--- test/board_test.rb
+++ test/board_test.rb
@@ -40,6 +40,21 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_placement?(sub, ["A2", "A3", "A4"])
     assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
     assert_equal true, board.valid_placement?(sub, ["D1", "D2"])
+
+  end
+
+  def test_it_has_consecutive_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    sub = Ship.new("Submarine", 2)
+
+    assert_equal false, board.consecutive_coordinates?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.consecutive_coordinates?(sub, ["A1", "C1"])
+    assert_equal false, board.consecutive_coordinates?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.consecutive_coordinates?(sub, ["C1", "B1"])
+
+    assert_equal true, board.consecutive_coordinates?(cruiser, ["A1", "A2", "A3"])
+    assert_equal true, board.consecutive_coordinates?(sub, ["C1", "D1"])
   end
 
 end
