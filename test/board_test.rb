37d34d143234800da3da99d7ae801cb34ec15703
Austin Aspaas <araspaas@live.com>
2020-07-08 20:42:23 -0600

    Add render board tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -90,4 +90,47 @@ class BoardTest < Minitest::Test
     board.place(cruiser, ["A1", "A2", "A3"])
     assert_equal false, board.valid_placement?(sub, ["A1", "B1"])
   end
+
+  def test_it_can_render_board
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+    expected = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
+    assert_equal expected, board.render
+    expected2 = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+    assert_equal expected2, board.render(true)
+  end
+
+  def test_it_can_render_board_miss
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.cells["B1"].fire_upon
+    assert_equal "  1 2 3 4 \nA . . . . \nB M . . . \nC . . . . \nD . . . . \n", board.render
+  end
+
+  def test_it_can_render_board_hit
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.cells["A1"].fire_upon
+    board.cells["A2"].fire_upon
+
+    assert_equal "  1 2 3 4 \nA H H . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
+  end
+
+  def test_it_can_render_sunk
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.cells["A1"].fire_upon
+    board.cells["A2"].fire_upon
+    board.cells["A3"].fire_upon
+
+    assert_equal "  1 2 3 4 \nA X X X . \nB . . . . \nC . . . . \nD . . . . \n", board.render
+  end
 end
