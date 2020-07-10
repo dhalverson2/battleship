895dea3f9ac8ebc0a5d20b0a4346ac5e50103951
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-09 20:19:03 -0600

    Add valid cpu coordinate array test

--- test/game_play_test.rb
+++ test/game_play_test.rb
@@ -25,8 +25,14 @@ class GamePlayTest < Minitest::Test
   def test_it_can_create_cpu_coordinates
     game_play = GamePlay.new
 
-    expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-    assert_equal expected, game_play.create_cpu_coordinate_array
+    assert_equal Array, game_play.create_cpu_coordinate_array_cruiser.class
+  end
+
+  def test_it_can_create_valid_cpu_coordinate_array
+    game_play = GamePlay.new
+
+    expected = [["A1", "A2", "A3"], ["A1", "B1", "C1"], ["A2", "A3", "A4"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["B1", "B2", "B3"], ["B1", "C1", "D1"], ["B2", "B3", "B4"], ["B2", "C2", "D2"], ["B3", "C3", "D3"], ["B4", "C4", "D4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"]]
+    assert_equal expected, game_play.select_cpu_valid_coordinate_array_cruiser
   end
 
 end
