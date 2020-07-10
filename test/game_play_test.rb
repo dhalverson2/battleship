3cca9c1aa0786e74477e10ea41600327479e26db
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-09 19:47:14 -0600

    Add game play create cpu coordinates test

--- test/game_play_test.rb
+++ test/game_play_test.rb
@@ -15,5 +15,18 @@ class GamePlayTest < Minitest::Test
     assert_instance_of GamePlay, game_play
   end
 
+  # def test_it_displays_welcome_message
+  #   game_play = GamePlay.new
+  #
+  #   assert_output(/"Welcome to BATTLESHIP"
+  #   "Enter p to play. Enter q to quit."/), game_play.welcome_message
+  # end
+
+  def test_it_can_create_cpu_coordinates
+    game_play = GamePlay.new
+
+    expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    assert_equal expected, game_play.create_cpu_coordinate_array
+  end
 
 end
