3c40cbf1b56abf0eccd2ded76baa14736c44063f
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-09 20:19:24 -0600

    Add game play select valid coordinate method cpu

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -4,6 +4,8 @@ class GamePlay
   def initialize
     @human_player = Player.new("Human")
     @cpu_player = Player.new("CPU")
+    @cpu_cruiser = Ship.new("Cruiser", 3)
+    @cpu_sub = Ship.new("Submarine", 2)
   end
 
   def welcome_message
@@ -19,11 +21,15 @@ class GamePlay
   # def cpu_ship_place
   # end
 
-  def create_cpu_coordinate_array
-    @cpu_player.board.cells.keys
+  def create_cpu_coordinate_array_cruiser
+    @cpu_player.board.cells.keys.permutation(3).to_a
   end
 
-  def
+  def select_cpu_valid_coordinate_array_cruiser
+    create_cpu_coordinate_array_cruiser.select do |coordinate|
+      @cpu_player.board.valid_placement?(@cpu_cruiser, coordinate)
+    end
+  end
 
   def end_game
   end
