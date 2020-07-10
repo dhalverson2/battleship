81776b4f191c3aa2223190a639dddfb734042673
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-09 20:25:51 -0600

    Add select valid coordinate test

--- test/game_play_test.rb
+++ test/game_play_test.rb
@@ -22,17 +22,22 @@ class GamePlayTest < Minitest::Test
   #   "Enter p to play. Enter q to quit."/), game_play.welcome_message
   # end
 
-  def test_it_can_create_cpu_coordinates
+  def test_it_can_create_cpu_coordinates_array
     game_play = GamePlay.new
 
     assert_equal Array, game_play.create_cpu_coordinate_array_cruiser.class
+    assert_equal Array, game_play.create_cpu_coordinate_array_sub.class
   end
 
-  def test_it_can_create_valid_cpu_coordinate_array
+  def test_it_can_select_valid_cpu_coordinate_array
     game_play = GamePlay.new
 
-    expected = [["A1", "A2", "A3"], ["A1", "B1", "C1"], ["A2", "A3", "A4"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["B1", "B2", "B3"], ["B1", "C1", "D1"], ["B2", "B3", "B4"], ["B2", "C2", "D2"], ["B3", "C3", "D3"], ["B4", "C4", "D4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"]]
-    assert_equal expected, game_play.select_cpu_valid_coordinate_array_cruiser
+    expected_cruiser = [["A1", "A2", "A3"], ["A1", "B1", "C1"], ["A2", "A3", "A4"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["B1", "B2", "B3"], ["B1", "C1", "D1"], ["B2", "B3", "B4"], ["B2", "C2", "D2"], ["B3", "C3", "D3"], ["B4", "C4", "D4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"]]
+    expected_sub = [["A1", "A2"], ["A1", "B1"], ["A2", "A3"], ["A2", "B2"], ["A3", "A4"], ["A3", "B3"], ["A4", "B4"], ["B1", "B2"], ["B1", "C1"], ["B2", "B3"], ["B2", "C2"], ["B3", "B4"], ["B3", "C3"], ["B4", "C4"], ["C1", "C2"], ["C1", "D1"], ["C2", "C3"], ["C2", "D2"], ["C3", "C4"], ["C3", "D3"], ["C4", "D4"], ["D1", "D2"], ["D2", "D3"], ["D3", "D4"]]
+    assert_equal expected_cruiser, game_play.select_cpu_valid_coordinate_array_cruiser
+    assert_equal expected_sub, game_play.select_cpu_valid_coordinate_array_sub
   end
+
+  
 
 end
